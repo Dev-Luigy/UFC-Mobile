e735a0cd3701a0cb681e695e6b941d6e00c93325
Dev-Luigy <luigygabriel18a@gmail.com>
2024-11-04 13:03:05 -0300

    chore: Completed activity 1.

--- FirstActivity/app/src/main/java/com/example/firstactivity/MainActivity.kt
+++ FirstActivity/app/src/main/java/com/example/firstactivity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.firstactivity
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
 
-    Log.d("LifecycleDemo", "onCreate chamado")
+    Log.d("FirstActivity", "onCreate chamado")
     Toast.makeText(this, "onCreate chamado", Toast.LENGTH_SHORT).show()
 
     setContentView(R.layout.activity_main)
@@ -20,5 +22,47 @@ class MainActivity : AppCompatActivity() {
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
       insets
     }
+  };
+
+  override fun onStart() {
+    super.onStart()
+
+    // Mensagem de log e Toast para onStart
+    Log.d("FirstActivity", "onStart chamado")
+    Toast.makeText(this, "onStart chamado", Toast.LENGTH_SHORT).show()
+  };
+
+  override fun onResume() {
+    super.onResume();
+
+    // Mensagem de log e Toast para onStart
+    Log.d("FirstActivity", "onResume chamado")
+    Toast.makeText(this, "onResume chamado", Toast.LENGTH_SHORT).show()
   }
+
+  override fun onPause() {
+    super.onPause()
+
+    // Mensagem de log e Toast para onStart
+    Log.d("FirstActivity", "onPause chamado")
+    Toast.makeText(this, "onPause chamado", Toast.LENGTH_SHORT).show()
+  }
+
+  override fun onStop() {
+    super.onStop()
+
+    // Mensagem de log e Toast para onStart
+    Log.d("FirstActivity", "onStop chamado")
+    Toast.makeText(this, "onStop chamado", Toast.LENGTH_SHORT).show()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+
+    // Mensagem de log e Toast para onStart
+    Log.d("FirstActivity", "onDestroy chamado")
+    Toast.makeText(this, "onDestroy chamado", Toast.LENGTH_SHORT).show()
+  }
+
+
 }
