f584318e5d0e592880c0764456cd2f6fb358120d
Dev-Luigy <luigygabriel18a@gmail.com>
2024-11-14 16:32:11 -0300

    chore: Added activity 4(Profile App).

---
+++ ProfileApp/app/src/main/java/com/example/profileapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.profileapp
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    // Inicializando componentes
+    val profileImage = findViewById<ImageView>(R.id.profileImage)
+    val nameText = findViewById<TextView>(R.id.nameText)
+    val descriptionText = findViewById<TextView>(R.id.descText)
+    val currentJobText = findViewById<TextView>(R.id.jobText)
+    val experienceLayout = findViewById<LinearLayout>(R.id.experienceLayout)
+
+    nameText.text = "Ana Silva"
+    descriptionText.text = "Desenvolvedora de software com 5 anos de experiência."
+    currentJobText.text = "Emprego Atual: Engenheira de Software na TechX"
+
+    // Lista de experiências
+    val experiencias = listOf(
+      "Analista de Sistemas - Empresa A",
+      "Desenvolvedora Júnior - Empresa B",
+      "Estagiária - Empresa C"
+    )
+
+    // Adicionando experiências dinamicamente
+    for (experiencia in experiencias) {
+      val textView = TextView(this)
+      textView.text = experiencia
+      textView.textSize = 16f
+      experienceLayout.addView(textView)
+    }
+
+    profileImage.setOnClickListener {
+      Toast.makeText(this, "Foto de perfil de Ana Silva", Toast.LENGTH_SHORT).show()
+    }
+  }
+}
